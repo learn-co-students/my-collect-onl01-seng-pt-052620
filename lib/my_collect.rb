1753f0d5e4d5cb8bcf1b96c1a5c61c764abdecad
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-06 22:09:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+#def my_collect(array) do |x|
+#  i = 0
+#  while my_collect < i = collection[i]
+#  yield()
+  # i = i += 1
+#  end
+def my_collect(array)
+  i = 0
+  name_collection = [languages]
+  while i < array.length
+    #name_collection.push yield(array[i])
+  name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
 
-
+my_collect(list) {|i| i.split(" ").first}
