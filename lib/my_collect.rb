49e9c09d2ff30d4ff8be0ae4f7af70735ef11ca1
mahdesignstudio <mahdesignstudio@gmail.com>
2020-06-06 21:58:28 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
 
+def my_collect(empty_array)
+    collection = []
+    counter = 0 
+    while counter < empty_array.length
+        collection << yield(empty_array[counter])
+    counter += 1
+    end 
+    collection
+end 
 
