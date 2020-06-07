64ea0ad5dc94fa03fc33db279de74f21fa71b82f
Emily Jones <emjones829@gmail.com>
2020-06-07 15:18:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(students)
+  students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+  empty = []
+  counter = 0
+  students.split(' ').first do |x|
+    x
+  end
+  empty.push(students)
+end
